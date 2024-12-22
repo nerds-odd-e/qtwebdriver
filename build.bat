aa20c7f5253608f2cd50f0bbeedec2edf7852a5a
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-12-22 20:25:51 +0800

    :sparkles: update build.bat

--- build.bat
+++ build.bat
@@ -1,14 +1,12 @@
 rem @ECHO OFF
 
-rd /S /Q out out-rel
-
 python generate_wdversion.py
 
 rem gyp --depth . -G output_dir=. -D platform=desktop -D mode=release --generator-output=out/ wd.gyp
+rd /S /Q build out
 gyp --depth . -f msvs -G msvs_version=2015 -G output_dir=. -D platform=desktop -D mode=debug --generator-output=out/ wd.gyp
-
 devenv .\out\wd.sln /rebuild "Default|x64"
 
+rd /S /Q build out-rel
 gyp --depth . -f msvs -G msvs_version=2015 -G output_dir=. -D platform=desktop -D mode=release --generator-output=out-rel/ wd.gyp
-
 devenv .\out-rel\wd.sln /rebuild "Default|x64"
